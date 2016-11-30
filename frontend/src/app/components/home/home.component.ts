4e12901810a19c9bf98f17d27cb5da3102a2495a
Dmytro Borysov <dmytrob@backbase.com>
2016-11-30 13:43:49 +0200

    Removed unneeded stuff from Home component

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,12 +1,4 @@
-import { Component, Inject } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
-
-import { IAppStore } from '../../../appStore';
-
-import { IProduct } from '../../models/product';
-
-import { INotificationsService } from '../../services/notifications.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-home',
@@ -14,14 +6,5 @@ import { INotificationsService } from '../../services/notifications.service';
     styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-    public items: Observable<IProduct[]>;
-
-    constructor(
-        private _store: Store<IAppStore>,
-        @Inject(INotificationsService) private _notificationService: INotificationsService
-    ) {
-        this._notificationService.success('Home page is opened!');
-
-        this.items = _store.select<IProduct[]>('cart');
-    }
+    constructor() { }
 }
