9d59664681c142b5d2afcce4c79d254213e8fa19
Dmytro Borysov <dmytrob@backbase.com>
2017-03-10 12:50:49 +0200

    fixing lint errors

--- frontend/src/app/reducers/cart.ts
+++ frontend/src/app/reducers/cart.ts
@@ -6,7 +6,7 @@ import * as catalog from '../actions/catalog';
 import { ICartPositionsDetails } from '../models';
 
 export interface IState {
-    cartItems: ICartPositionsDetails[]
+    cartItems: ICartPositionsDetails[];
 }
 
 export const initialState = { cartItems: [] };
@@ -16,18 +16,18 @@ export const reducer: ActionReducer<IState> = (state: IState = initialState, act
         case cart.ActionTypes.LOAD_SUCCESS:
             return {
                 cartItems: action.payload
-            }
+            };
 
         case cart.ActionTypes.LOAD_DETAILS_SUCCESS:
             return {
                 cartItems: state.cartItems.map(cartItem => {
-                    const cartItemDetails = action.payload.find(cartItemDetails => cartItemDetails.id === cartItem.id);
+                    const cartItemDetails = action.payload.find(itemDetails => itemDetails.id === cartItem.id);
 
                     return cartItemDetails
                         ? { ...cartItem, ...cartItemDetails, detailsLoaded: true }
                         : cartItem;
                 })
-            }
+            };
 
         case cart.ActionTypes.ADD_QUANTITY:
             const existingItem = state.cartItems.find(cartItem => cartItem.id === action.payload.id);
@@ -39,12 +39,12 @@ export const reducer: ActionReducer<IState> = (state: IState = initialState, act
                             ? { ...cartItem, quantity: (cartItem.quantity || 0) + (action.payload.quantity || 1) }
                             : cartItem)
                     : [...state.cartItems, action.payload]
-            }
+            };
 
         case cart.ActionTypes.REMOVE_QUANTITY:
-            const cartItem = state.cartItems.find(cartItem => cartItem.id === action.payload.id);
+            const cartItem = state.cartItems.find(item => item.id === action.payload.id);
 
-            if (!cartItem) return state;
+            if (!cartItem) { return state; };
 
             const shouldRemoveItem = !action.payload.quantity
                 || action.payload.quantity >= cartItem.quantity;
@@ -52,14 +52,16 @@ export const reducer: ActionReducer<IState> = (state: IState = initialState, act
             return shouldRemoveItem
                 ? reducer(state, new cart.RemoveItemAction({ id: action.payload.id }))
                 : {
-                    cartItems: state.cartItems.map(cartItem => cartItem.id === action.payload.id ? { ...cartItem, quantity: cartItem.quantity - action.payload.quantity } : cartItem)
+                    cartItems: state.cartItems.map(item => item.id === action.payload.id
+                        ? { ...item, quantity: item.quantity - action.payload.quantity }
+                        : item)
                 };
 
         case cart.ActionTypes.REMOVE_ITEM:
         case catalog.ActionTypes.DELETE_SUCCESS:
             return {
-                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
-            }
+                cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
+            };
 
         case cart.ActionTypes.REMOVE_ALL:
             return initialState;
